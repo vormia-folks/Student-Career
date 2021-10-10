5c9cdf80baa91bcbd22744f18e0f7c06afa355f3
Josh L. Minga <joshlminga@gmail.com>
2021-10-10 14:10:31 +0300

    Base URL changed to Assets URL

--- views/students/includes/head.php
+++ views/students/includes/head.php
@@ -4,7 +4,17 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Meta Tags -->
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+    <meta name="author" content="">
+    <meta name="robots" content="">
+
+    <!-- Title -->
+    <title><?= $site_title; ?></title>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
@@ -12,8 +22,9 @@
     <!-- Font Icons -->
     <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" onload="this.onload=null;this.rel='stylesheet'">
 
-    <link rel="stylesheet" href="<?= $base_url; ?>/assets/students/css/style.min.css?<?= time(); ?>">
-    <title><?= $site_title; ?></title>
+    <!-- Style -->
+    <link rel="stylesheet" href="<?= $asset_url; ?>/students/css/style.min.css?<?= time(); ?>">
+
 </head>
 
 <body>
