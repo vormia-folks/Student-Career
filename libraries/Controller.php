9bf4524efbb2c6da0219666eb52688663f1112c3
Josh L. Minga <joshlminga@gmail.com>
2021-10-10 13:53:48 +0300

    Base URL and Addtional Support methods added

--- libraries/Controller.php
+++ libraries/Controller.php
@@ -4,10 +4,50 @@ require_once 'libraries/View.php';
 
 class Controller
 {
-
     public function __construct()
     {
         // Initiate the View
         $this->view = new View;
+    }
+
+    /**
+     * URL to a Page URL
+     */
+    public function base_url($set_url = null)
+    {
+        // Include Config
+        include 'libraries/Config.php';
+
+        // Set URL
+        if (!is_null($set_url)) {
+            return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . $config['base_url'] . '/' . $set_url;
+        } elseif (is_null($set_url)) {
+            return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . $config['base_url'];
+        }
+    }
+
+    /**
+     * URL to a Assets Access 
+     */
+    public function asset_url($set_path = null)
+    {
+        // Include Config
+        include 'libraries/Config.php';
+
+        // Set URL
+        if (!is_null($set_path)) {
+            return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . $config['base_url'] . '/' . $config['assets'] . '/' . $set_path;
+        } elseif (is_null($set_url)) {
+            return $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['HTTP_HOST'] . '/' . $config['base_url'] . '/' . $config['assets'];
+        }
+    }
+
+    /**
+     * Check If Array Is Associative Array
+     */
+    function isAssoc(array $arr)
+    {
+        if (array() === $arr) return false;
+        return array_keys($arr) !== range(0, count($arr) - 1);
     }
 }
--- libraries/Model.php
+++ libraries/Model.php
@@ -2,4 +2,7 @@
 
 class Model
 {
+    public function __construct()
+    {
+    }
 }
