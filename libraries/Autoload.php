a2928fa65a5fa839081fb6a380065c70a36ccfb1
Josh L. Minga <joshlminga@gmail.com>
2021-10-10 13:55:54 +0300

    Autoload Library Created to help loading Models and helpers

---
+++ libraries/Autoload.php
@@ -0,0 +1,9 @@
+<?php
+
+// Include Global Requred Library Here
+require_once 'libraries/Controller.php';
+require_once 'libraries/Plural.php';
+
+// Include Global Requred Models Here
+require_once 'models/Load.php';
+require_once 'models/Notify.php';
