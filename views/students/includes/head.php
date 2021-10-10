e9ed621aa253a66d9dffe586d7a1eb28b5da0713
Josh L. Minga <joshlminga@gmail.com>
2021-10-10 13:56:43 +0300

    Page Views

---
+++ views/students/includes/head.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+
+    <!-- Font Icons -->
+    <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" onload="this.onload=null;this.rel='stylesheet'">
+
+    <link rel="stylesheet" href="<?= $base_url; ?>/assets/students/css/style.min.css?<?= time(); ?>">
+    <title><?= $site_title; ?></title>
+</head>
+
+<body>
---
+++ views/students/pages/home.php
@@ -0,0 +1,9 @@
+<h1>Home Page</h1>
+<p><?= $message; ?></p>
+<h4><?= $base; ?></h4>
+
+<pre>
+    <?php print_r($_SERVER); ?>
+</pre>
+
+<!-- Optional JavaScript; choose one of the two! -->
