36cc5880844ad5d06a3d56fc9c3a06ba09630f47
Josh L. Minga <joshlminga@gmail.com>
2021-10-10 14:59:54 +0300

    Logo and Menu

--- views/students/includes/head.php
+++ views/students/includes/head.php
@@ -22,9 +22,56 @@
     <!-- Font Icons -->
     <link rel="preload" as="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" onload="this.onload=null;this.rel='stylesheet'">
 
+    <!-- Fonts Family -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Festive&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500&family=Roboto:ital,wght@0,100;0,300;0,400;1,100;1,300;1,400;1,500&display=swap" rel="stylesheet">
+
     <!-- Style -->
     <link rel="stylesheet" href="<?= $asset_url; ?>/students/css/style.min.css?<?= time(); ?>">
 
 </head>
 
 <body>
+
+    <header class="main-nav">
+        <div class="container">
+            <nav class="navbar navbar-expand-lg navbar-light fixed-top">
+                <div class="container">
+
+                    <!-- Logo Here -->
+                    <a class="navbar-brand" href="#">
+                        <img src="<?= $asset_url; ?>/students/img/logo/logo_1.png" alt="">
+                    </a>
+
+                    <!-- Navigation Start -->
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                        <ul class="navbar-nav ms-auto navbar-nav-scroll">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="<?= $base_url; ?>">Home</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Features</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Pricing</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown link
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                                    <li><a class="dropdown-item" href="#">Action</a></li>
+                                    <li><a class="dropdown-item" href="#">Another action</a></li>
+                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
--- views/students/pages/home.php
+++
@@ -1,9 +0,0 @@
-<h1>Home Page</h1>
-<p><?= $message; ?></p>
-<h4><?= $base; ?></h4>
-
-<pre>
-    <?php print_r($_SERVER); ?>
-</pre>
-
-<!-- Optional JavaScript; choose one of the two! -->
